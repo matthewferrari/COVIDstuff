9f4e84c38ebeef4f10644974d2c7e18b2f4b758a
matthewferrari <matthewjferrari@gmail.com>
2020-03-25 08:19:37 -0400

    Added additional plot
    added additional plot with separate trends for cases and testing over time.

--- testing_plot.R
+++ testing_plot.R
@@ -22,6 +22,7 @@ pa <- dt %>%
 
 # plot the number of tests per state over time
 # size of point indicates the test-positive rate
+# dashed line is number of tests in PA for reference
   gg <- ggplot(dt, aes(x=date,y=tests,size=test_pos,color=state)) +
     geom_point() + 
     scale_y_continuous(trans='log10') +
@@ -32,4 +33,18 @@ pa <- dt %>%
   
   gg
 
+# plot the number of tests per state over time and the number of postive cases over time
+# if these lines are diverging, that implies that testing is increasing faster than cases are increasing
+# if the lines are converging, that implies that cases are increasing faster than testing
+# this could be used to provide an index of expected trend over time in cases
+# dashed line is number of tests in PA for reference
+ff <- ggplot(dt, aes(x=date,y=tests,color=state)) +
+  geom_point() + 
+  scale_y_continuous(trans='log10') +
+  geom_hline(yintercept=(pa$positive[1] + pa$negative[1]), linetype="dashed", color = "red") +
+  facet_wrap(~state,ncol=10) + 
+  scale_size_continuous(name = "% test positive") +
+  ylab("cumulative tests conducted")+
+  geom_point(data=dt,aes(x=date,y=positive,color=state),shape=15)
 
+ff
